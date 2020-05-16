a1c44de9d9003dae0a0367eb90c9f790644f2fe5
Rachel <rachel17.22@gmail.com>
2020-05-16 15:56:14 -0600

    Add Thunk for  piece of state

--- src/reducers/guessedWordsReducer.js
+++ src/reducers/guessedWordsReducer.js
@@ -1,9 +1,16 @@
+import { actionTypes } from '../actions';
+
 /**
  * @function guessedWordsReducer
  * @param {array} state -
  * @param {object} action -
  * @returns {array} -
  */
-export default (state, action) => {
-  return null;
+export default (state=[], action) => {
+  switch(action.type) {
+    case actionTypes.GUESS_WORD:
+      return [...state, action.payload]
+    default:
+      return state;
+  }
 }
